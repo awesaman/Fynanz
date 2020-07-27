837bbd1b3dd3d018b993493d895b784473304fb6
Aman Singh <aman@little-mac.local>
2020-07-27 11:48:16 -0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ connection.once('open', () => {
 app.use('/api/standard', require('./routes/api/standard'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static('../build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
   });
