89e34927f2c9153cf52e38d3701067062465c66a
Aman Singh <aman@little-mac.local>
2020-07-27 01:12:00 -0500

    added stock plot

--- src/components/Investments/Investments.js
+++ src/components/Investments/Investments.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import Plot from 'react-plotly.js';
 import nestedBullets from '../../utils/nestedBullets';
 import Spinner from '../General/Spinner';
 
 class Investments extends Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, data: {} };
+    this.state = { loading: true, data: {}, xvalues: [], yvalues: [] };
   }
 
   componentWillMount() {
@@ -17,6 +18,34 @@ class Investments extends Component {
       })
       .catch(error => {
         console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.stockAPI();
+  }
+
+  stockAPI() {
+    const ptr = this;
+    const API_KEY = '78c41dd4ecmsh627d880cd3749dfp1d1c15jsncf17081dad76';
+    let ticker = 'AAPL';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=compact&apikey=${API_KEY}`;
+    let x = [];
+    let y = [];
+
+    fetch(API_Call)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var key in data['Time Series (Daily)']) {
+          x.push(key);
+          y.push(data['Time Series (Daily)'][key]['1. open']);
+        }
+        ptr.setState({
+          xvalues: x,
+          yvalues: y,
+        });
       });
   }
 
@@ -37,6 +66,19 @@ class Investments extends Component {
             <ul>
               {this.state.data.subtopics[1].list.map(s => nestedBullets(s))}
             </ul>
+            <h3>An Example Stock</h3>
+            <Plot
+              data={[
+                {
+                  x: this.state.xvalues,
+                  y: this.state.yvalues,
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: { color: 'blue' },
+                },
+              ]}
+              layout={{ title: 'Apple' }}
+            />
           </div>
         )}
       </Fragment>
